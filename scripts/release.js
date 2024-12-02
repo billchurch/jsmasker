5d6e787fc81bd0b70f4cfb9af5c7a69fc62f7eb9
Bill Church <billchurch@users.noreply.github.com>
2024-12-02 10:58:07 -0500

    chore: dev env

--- scripts/release.js
+++ scripts/release.js
@@ -2,11 +2,28 @@ const { execSync } = require('child_process')
 const path = require('path')
 const fs = require('fs')
 
-// Get next version without bumping
-const nextVersion = execSync('npx standard-version --dry-run --skip.bump')
-  .toString()
-  .match(/bumping version in package\.json from .* to (.*)/)[1]
-  .trim()
+// Get next version using conventional-recommended-bump
+const getNextVersion = () => {
+  const pkg = require('../package.json')
+  const currentVersion = pkg.version
+  const recommendedBump = execSync(
+    'npx conventional-recommended-bump --preset angular'
+  )
+    .toString()
+    .trim()
+
+  const [major, minor, patch] = currentVersion.split('.')
+  switch (recommendedBump) {
+    case 'major':
+      return `${Number(major) + 1}.0.0`
+    case 'minor':
+      return `${major}.${Number(minor) + 1}.0`
+    default:
+      return `${major}.${minor}.${Number(patch) + 1}`
+  }
+}
+
+const nextVersion = getNextVersion()
 
 // Build webpack with new version to update README
 execSync('webpack --config scripts/webpack.config.js', {
